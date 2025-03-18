5bc543a337ef6c67e08bd4aa6327e043ab21f486
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2025-03-18 22:40:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=ARSEN;Initial Catalog=VegetableFruits;Integrated Security=True";
+
+
+            using (SqlConnection sql_connect = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    sql_connect.Open();
+                    Console.WriteLine("Поддключение к базе данных успешко.");
+                    Console.WriteLine($"Сервер: {sql_connect.DataSource}");
+                    Console.WriteLine($"База данных: {sql_connect.Database}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error " + ex.Message);
+                }
+                finally
+                {
+                    sql_connect.Close();
+                    Console.WriteLine("Подключение закрыто.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE VegFruTable 
+(
+    ID INT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Type VARCHAR(10) NOT NULL,
+    Color VARCHAR(50) NOT NULL,
+    Calories INT NOT NULL
+);
+
